1896dabc9b390190c10c95e37df17ba51386b397
rakshana2300 <rakshana2300@gmail.com>
2021-06-25 13:25:08 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
+
+app.set('view engine','ejs');
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser : true});
+
+const itemSchema = {
+    name : String
+};
+
+const Item = mongoose.model("item", itemSchema);
+
+
+
+app.get("/",function(req,res){
+    var options = {weekday: 'long', day:'numeric',month:'long'};
+    var today = new Date().toLocaleDateString('en-US',options);
+    Item.find({},function(err,listItems){
+        if(listItems.length === 0){
+            res.redirect("/");
+        }
+        
+        else{
+            res.render("list",{day:today,items:listItems});
+        }
+    })
+    
+});
+
+app.post("/",function(req,res){
+    const item=req.body.work;
+    const newItem = new Item({
+        name:item
+    });
+    newItem.save();
+    res.redirect('/');
+});
+
+
+app.post("/delete",function(req,res){
+    const index=req.body.done;
+    Item.findByIdAndRemove(index,function(err){
+        if(!err){
+            res.redirect('/');
+        }
+    });
+    
+});
+/*app.get("/delete", function(req,res){
+    res.render("list",{removeItems:removeList});
+})*/
+
+app.listen(7000,function(){
+    console.log("Listening");
+});
